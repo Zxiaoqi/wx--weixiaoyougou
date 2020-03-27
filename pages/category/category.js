6ef598457ff55b1a0c70a71e037b02bd33b35364
zxiaoqi <mrzeng18229230299@163.com>
2020-03-27 12:08:42 +0800

    首页commit

--- pages/category/category.js
+++ pages/category/category.js
@@ -1,66 +1,77 @@
 // pages/category/category.js
+const order = ["demo1", "demo2", "demo3"];
+
 Page({
+	/**
+	 * 页面的初始数据
+	 */
+	data: {
+		toView: "green",
+		scrollTop: 0
+	},
+	scroll(e) {
+    // console.log(e);
+		// console.log(this.data.toView);
+	},
+	scrollToTop() {
+		this.setAction({
+			scrollTop: 0
+		});
+	},
+	tap() {
+		for (let i = 0; i < order.length; ++i) {
+			if (order[i] === this.data.toView) {
+				this.setData({
+					toView: order[i + 1],
+					scrollTop: (i + 1) * 200
+				});
+				break;
+			}
+		}
+	},
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+	tapMove() {
+		this.setData({
+			scrollTop: this.data.scrollTop + 10
+		});
+	},
+	/**
+	 * 生命周期函数--监听页面加载
+	 */
+	onLoad: function(options) {},
 
-  },
+	/**
+	 * 生命周期函数--监听页面初次渲染完成
+	 */
+	onReady: function() {},
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+	/**
+	 * 生命周期函数--监听页面显示
+	 */
+	onShow: function() {},
 
-  },
+	/**
+	 * 生命周期函数--监听页面隐藏
+	 */
+	onHide: function() {},
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+	/**
+	 * 生命周期函数--监听页面卸载
+	 */
+	onUnload: function() {},
 
-  },
+	/**
+	 * 页面相关事件处理函数--监听用户下拉动作
+	 */
+	onPullDownRefresh: function() {},
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+	/**
+	 * 页面上拉触底事件的处理函数
+	 */
+	onReachBottom: function() {},
 
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
-})
+	/**
+	 * 用户点击右上角分享
+	 */
+	onShareAppMessage: function() {}
+});
--- pages/index/index.js
+++ pages/index/index.js
@@ -6,28 +6,20 @@ Page({
 		swiperlist: [],
 		catitemslist: [],
 		productlist: [],
+		topfixed:100
 	},
 	onLoad() {
-		// this.animate('#container', [
-		//   { opacity: 0.0, rotate: 90 },
-		//   { opacity: 0.5, rotate: 45},
-		//   { opacity: 1.0, rotate: 0 },
-		//   ], 5000, function () {
-		//     this.clearAnimation('#container', { opacity: true, rotate: true }, function () {
-		//       console.log("清除了#container上的opacity和rotate属性")
-		//     })
-		// }.bind(this))
 		app.axios({
 				url: "/home/swiperdata"
-			})
-			.then(res => {
-				// console.log(res);
-				const { message, meta } = res;
-        if (meta.status === 200) {
-					this.setData({
-						swiperlist: message
-					});
-				}
+		})
+		.then(res => {
+			// console.log(res);
+			const { message, meta } = res;
+			if (meta.status === 200) {
+				this.setData({
+					swiperlist: message
+				});
+			}
 		});
 		// wx.request({
 		//   url: "https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata",
@@ -36,35 +28,60 @@ Page({
 		//     console.log(this);
 		// 	}
 		// });
-		app.axios({
-				url: "/home/catitems"
-			})
-			.then(res => {
-				// console.log(res);
-				const { message, meta } = res;
-        if (meta.status === 200) {
-					this.setData({
-						catitemslist: message
-					});
-				}
+		app
+		.axios({
+			url: "/home/catitems"
+		})
+		.then(res => {
+			// console.log(res);
+			const { message, meta } = res;
+			if (meta.status === 200) {
+				this.setData({
+					catitemslist: message
+				});
+			}
 		});
 
-		app.axios({
-				url: "/home/floordata"
-			})
-			.then(res => {
-				console.log(res);
-				const { message, meta } = res;
-        if (meta.status === 200) {
-          let data = message.map(item => {
-            item.productleft = item.product_list.shift();
-            return item
-          });
-          // console.log(data);
-					this.setData({
-						productlist: data
-					});
-        }
+		app
+		.axios({
+			url: "/home/floordata"
+		})
+		.then(res => {
+			// console.log(res);
+			const { message, meta } = res;
+			if (meta.status === 200) {
+				let data = message.map(item => {
+					item.productleft = item.product_list.shift();
+					return item;
+				});
+				// console.log(data);
+				this.setData({
+					productlist: data,
+					topfixed:100
+				});
+			}
 		});
+	},
+	//跳转列表页
+	goToPagelist(e) {
+		// console.log(e);
+		const { type, url } = e.currentTarget.dataset;
+		let src = url.replace("?", "/goods_list?");
+		// let toUrl = src[0] + '/goods_list?' + src[1]
+		if (type === "navigate") {
+			wx.navigateTo({
+				url: src
+			});
+		}
+	},
+	onPullDownRefresh() {
+		this.setData({
+			swiperlist: [],
+			catitemslist: [],
+			productlist: [],
+			topfixed: 0
+		});
+		this.onLoad();
+		wx.stopPullDownRefresh()
 	}
 });
