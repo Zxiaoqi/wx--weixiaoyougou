242f6c1c8eb5f8377ed7ff1a73d8d42fe8085f01
zxiaoqi <mrzeng18229230299@163.com>
2020-03-28 17:16:32 +0800

    backtop组件

--- pages/goods_list/goods_list.js
+++ pages/goods_list/goods_list.js
@@ -13,18 +13,19 @@ Page({
 			pagenum: 1,
 			pagesize: 10
 		},
-		goodData: []
+		goodData: [],
+		showBack:false
 	},
 	getGoodsList() {
 		app
 			.axios({
 				url: "/goods/search",
-				data:this.data.params
+				data: this.data.params
 			})
 			.then(res => {
 				// console.log(res);
-        const { message, meta } = res;
-        total = message.total
+				const { message, meta } = res;
+				total = message.total;
 				if (meta.status === 200) {
 					this.setData({
 						goodData: [...this.data.goodData, ...message.goods]
@@ -35,7 +36,8 @@ Page({
 	}, //页面初始化 options为页面跳转所带来的参数
 	/**
 	 * 生命周期函数--监听页面加载
-	 */ onLoad: function(options) {
+	 */
+	onLoad: function (options) {
 		// console.log(options);
 		const { query, cid } = options;
 		if (query && cid) {
@@ -46,31 +48,37 @@ Page({
 		}
 		this.getGoodsList();
 	},
-
 	/**
 	 * 页面相关事件处理函数--监听用户下拉动作
 	 */
 	onPullDownRefresh() {
 		// console.log(this.data);
-    this.data.params.pagenum = 1
+		this.data.params.pagenum = 1;
 		this.setData({
 			goodData: []
 		});
 		this.getGoodsList();
 	},
-
 	/**
 	 * 页面上拉触底事件的处理函数
 	 */
-  onReachBottom() {
-    if (this.data.goodData.length == total) {
-      wx.showToast({
-        title: '已经到底了',
-        duration:1500
-      })
-    } else { 
-      this.data.params.pagenum ++
-      this.getGoodsList();
-    }
+	onReachBottom() {
+		if (this.data.goodData.length == total) {
+			wx.showToast({
+				title: "已经到底了",
+				duration: 1500
+			});
+		} else {
+			this.data.params.pagenum++;
+			this.getGoodsList();
+		}
+	},
+	onPageScroll(e) { 
+		// console.log(e);
+		if (e.scrollTop > 100) { 
+			this.setData({
+				showBack:true
+			});
+		}
 	}
 });
