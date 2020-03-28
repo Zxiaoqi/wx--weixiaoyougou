36f72bbc7165523e65fcfce8af1e201161941f9f
zxiaoqi <mrzeng18229230299@163.com>
2020-03-28 21:18:09 +0800

    商品详情

--- pages/goods_detail/goods_detail.js
+++ pages/goods_detail/goods_detail.js
@@ -1,65 +1,57 @@
 // pages/goods_detail/goods_detail.js
+const app = getApp()
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    goods_id: 0,
+    goodsDetail: {
+      pics:[]
+    }
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  getGoodsDetail() { 
+    app.axios({
+      url: "/goods/detail",
+      data: {
+        goods_id:this.data.goods_id
+      }
+    }).then(res => { 
+      console.log(res);
+      const { message, meta } = res
+      if (meta.status === 200) { 
+        this.setData({
+          goodsDetail:message
+        })
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  onLoad(options) {
+    const { goods_id } = options
+    // console.log(goods_id);
+    if (goods_id) { 
+      this.setData({
+				goods_id
+      });
+    }
+    this.getGoodsDetail()
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  //图片预览
+  previewImage(e) { 
+    // console.log(e);
+    const { img } = e.currentTarget.dataset
+    const pics = this.data.goodsDetail.pics.map(item => {
+      if (item.pics_big) { 
+        return item.pics_big
+      }
+    })
+    wx.previewImage({
+			current: img,
+			urls: pics
+		});
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
+  //页面相关事件处理函数--监听用户下拉动作
   onPullDownRefresh: function () {
 
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
+  //用户点击右上角分享
   onShareAppMessage: function () {
 
   }
