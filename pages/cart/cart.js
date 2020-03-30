b0c8365e8b6e3578e6bd7d61b0fdf271127d1646
zxiaoqi <mrzeng18229230299@163.com>
2020-03-30 18:48:30 +0800

    购物车商品数量加减

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -1,7 +1,7 @@
 // pages/cart/cart.js
 Page({
 	data: {
-		goods: [],
+		goods: []
 	},
 
 	onShow() {
@@ -11,15 +11,31 @@ Page({
 		});
 	},
 	handleSelect(e) {
-    const { isselect, index } = e.currentTarget.dataset;
-    // console.log(isselect,index);
-    if (!isselect) {
-      this.data.goods[index].isSelect = true
-    } else {
-      this.data.goods[index].isSelect = false;
-    }
-    // console.log(this.data.goods);
-    wx.setStorageSync("goods", this.data.goods);
-    this.onShow()
+		const { isselect, index } = e.currentTarget.dataset;
+		// console.log(isselect,index);
+		if (!isselect) {
+			this.data.goods[index].isSelect = true;
+		} else {
+			this.data.goods[index].isSelect = false;
+		}
+		// console.log(this.data.goods);
+		wx.setStorageSync("goods", this.data.goods);
+		this.onShow();
+	},
+	addNum(e) { 
+		const { index } = e.currentTarget.dataset;
+		
+			this.data.goods[index].number++;
+		wx.setStorageSync("goods", this.data.goods);
+		this.onShow();
+	},
+	lessNum(e) { 
+		const { index } = e.currentTarget.dataset;
+		let num = this.data.goods[index].number
+		if (num > 0) {
+			this.data.goods[index].number--;
+		}
+		wx.setStorageSync("goods", this.data.goods);
+		this.onShow();
 	}
 });
