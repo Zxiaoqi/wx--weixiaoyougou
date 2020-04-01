84157dd9a5a9e4cb1c08ca14a80f85786a375fdc
zxiaoqi <mrzeng18229230299@163.com>
2020-04-01 16:07:21 +0800

    单元格组件封装

---
+++ components/cell/cell.js
@@ -0,0 +1,23 @@
+// components/cell/cell.js
+Component({
+	properties: {
+		icon: {
+			type: String,
+			value: ""
+		},
+		title: {
+			type: String,
+			value: "单元格"
+		},
+		content: {
+			type: String,
+			value: ""
+		},
+    arrow: {
+      type: Boolean,
+      value:false
+    }
+	},
+	data: {},
+	methods: {}
+});
