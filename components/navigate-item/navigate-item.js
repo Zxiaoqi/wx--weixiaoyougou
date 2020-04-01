f6566e9032cd4fec010e9592d388cf0a7b5aebe4
zxiaoqi <mrzeng18229230299@163.com>
2020-04-01 15:09:54 +0800

    自定义navigate组件

---
+++ components/navigate-item/navigate-item.js
@@ -0,0 +1,19 @@
+// components/navigate-item/navigate-item.js
+Component({
+	properties: {
+		icon: {
+			type: String,
+			value: ""
+		},
+		text: {
+			type: String,
+			value: ""
+		},
+		title: {
+			type: String,
+			value: ""
+		}
+	},
+	data: {},
+	methods: {}
+});
